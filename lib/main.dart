59d33614cfd7083e406fff0139f414b31904fb1f
Matt Daley <mdaleyhk@gmail.com>
2019-12-02 14:54:33 +0000

    Remove tests and fix initial setup bugs

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Gets It Done'),
+    return Center(
+      child: Text(
+        'Gets It Done',
+        textDirection: TextDirection.ltr,
+      ),
     );
   }
 }
