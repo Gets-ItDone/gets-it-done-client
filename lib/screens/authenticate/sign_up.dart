cc3698230065dddabec9b48784a7d5462f17ad0b
Matt Daley <mdaleyhk@gmail.com>
2019-12-03 09:09:32 +0000

    Add password match check and save inputs in state. Render error to screen under button.

--- lib/screens/authenticate/sign_up.dart
+++ lib/screens/authenticate/sign_up.dart
@@ -10,6 +10,12 @@ class _SignUpState extends State<SignUp> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
 
+  // Text fields
+  String email = '';
+  String password = '';
+  String passwordConfirm = '';
+  String err = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +33,7 @@ class _SignUpState extends State<SignUp> {
         child: Column(
           children: <Widget>[
             SizedBox(
-              height: 40.0,
+              height: 20.0,
             ),
             TextFormField(
               validator: (value) =>
@@ -37,6 +43,9 @@ class _SignUpState extends State<SignUp> {
                   hintText: 'Please enter email.',
                   fillColor: Colors.white,
                   filled: true),
+              onChanged: (value) {
+                setState(() => email = value);
+              },
             ),
             SizedBox(
               height: 20.0,
@@ -50,6 +59,9 @@ class _SignUpState extends State<SignUp> {
                   hintText: 'Please enter password.',
                   fillColor: Colors.white,
                   filled: true),
+              onChanged: (value) {
+                setState(() => password = value);
+              },
             ),
             SizedBox(
               height: 20.0,
@@ -63,17 +75,35 @@ class _SignUpState extends State<SignUp> {
                   hintText: 'Please re-enter password.',
                   fillColor: Colors.white,
                   filled: true),
+              onChanged: (value) {
+                setState(() => passwordConfirm = value);
+              },
             ),
             RaisedButton(
               onPressed: () async {
-                await _auth.registerWithEmailAndPassword(
-                    'Me@you.com', 'password');
+                if (password != passwordConfirm) {
+                  setState(() => err = 'Passwords do not match');
+                } else if (_formKey.currentState.validate()) {
+                  dynamic signUpResult =
+                      await _auth.registerWithEmailAndPassword(email, password);
+
+                  if (signUpResult == null) {
+                    setState(() => err = 'Please enter a valid email');
+                  }
+                }
               },
               color: Colors.blue,
               child: Text('Register',
                   style: TextStyle(
                     color: Colors.white,
                   )),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              err,
+              style: TextStyle(color: Colors.red, fontSize: 14.0),
             )
           ],
         ),
