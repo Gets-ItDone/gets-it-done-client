d65e7c38a2cbbd16f8c3d0fe46923efc26953d0c
Matt Daley <mdaleyhk@gmail.com>
2019-12-03 09:40:39 +0000

    Change homescreen for testing Login()

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gets_it_done/screens/authenticate/login.dart';
 import 'package:gets_it_done/screens/authenticate/sign_up.dart';
 
 void main() => runApp(MyApp());
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SignUp(),
+      home: Login(),
     );
   }
 }
