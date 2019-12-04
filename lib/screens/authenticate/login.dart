429a02fb8839c99b04b4269a2153cc4f21d30a0b
Matt Daley <mdaleyhk@gmail.com>
2019-12-04 15:19:38 +0000

    Create loading page for login and signup.

--- lib/screens/authenticate/login.dart
+++ lib/screens/authenticate/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gets_it_done/services/auth.dart';
+import 'package:gets_it_done/shared/loading.dart';
 
 class Login extends StatefulWidget {
   final Function toggleView;
@@ -12,6 +13,7 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   String email = '';
   String password = '';
@@ -19,85 +21,90 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-        actions: <Widget>[
-          FlatButton(
-            child: Text('Register',
-                style: TextStyle(color: Colors.white, fontSize: 18.0)),
-            onPressed: () {
-              widget.toggleView();
-            },
-          ),
-        ],
-      ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 20.0,
+    return loading
+        ? Loading()
+        : Scaffold(
+            appBar: AppBar(
+              title: Text('Login'),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('Register',
+                      style: TextStyle(color: Colors.white, fontSize: 18.0)),
+                  onPressed: () {
+                    widget.toggleView();
+                  },
+                ),
+              ],
             ),
-            TextFormField(
-              validator: (value) =>
-                  value.isEmpty ? 'Please input your email.' : null,
-              decoration: InputDecoration(
-                  labelText: 'Email',
-                  hintText: 'Please enter email.',
-                  fillColor: Colors.white,
-                  filled: true),
-              onChanged: (value) {
-                setState(() => email = value);
-              },
+            body: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  TextFormField(
+                    validator: (value) =>
+                        value.isEmpty ? 'Please input your email.' : null,
+                    decoration: InputDecoration(
+                        labelText: 'Email',
+                        hintText: 'Please enter email.',
+                        fillColor: Colors.white,
+                        filled: true),
+                    onChanged: (value) {
+                      setState(() => email = value);
+                    },
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    validator: (value) =>
+                        value.isEmpty ? 'Please input your password.' : null,
+                    decoration: InputDecoration(
+                        labelText: 'password',
+                        hintText: 'Please enter password.',
+                        fillColor: Colors.white,
+                        filled: true),
+                    onChanged: (value) {
+                      setState(() => password = value);
+                    },
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  RaisedButton(
+                    onPressed: () async {
+                      if (_formKey.currentState.validate()) {
+                        setState(() => loading = true);
+                        dynamic loginResult = await _auth
+                            .signInWithEmailAndPassword(email, password);
+                        print(loginResult);
+                        if (loginResult == null) {
+                          setState(() {
+                            err = 'Something went wrong. Please try again.';
+                            loading = true;
+                          });
+                        }
+                      }
+                    },
+                    color: Colors.blue,
+                    child: Text('Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                        )),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    err,
+                    style: TextStyle(color: Colors.red, fontSize: 14.0),
+                  )
+                ],
+              ),
             ),
-            SizedBox(
-              height: 20.0,
-            ),
-            TextFormField(
-              obscureText: true,
-              validator: (value) =>
-                  value.isEmpty ? 'Please input your password.' : null,
-              decoration: InputDecoration(
-                  labelText: 'password',
-                  hintText: 'Please enter password.',
-                  fillColor: Colors.white,
-                  filled: true),
-              onChanged: (value) {
-                setState(() => password = value);
-              },
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            RaisedButton(
-              onPressed: () async {
-                if (_formKey.currentState.validate()) {
-                  dynamic loginResult =
-                      await _auth.signInWithEmailAndPassword(email, password);
-                  print(loginResult);
-                  if (loginResult == null) {
-                    setState(
-                        () => err = 'Something went wrong. Please try again.');
-                  }
-                }
-              },
-              color: Colors.blue,
-              child: Text('Login',
-                  style: TextStyle(
-                    color: Colors.white,
-                  )),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              err,
-              style: TextStyle(color: Colors.red, fontSize: 14.0),
-            )
-          ],
-        ),
-      ),
-    );
+          );
   }
 }
