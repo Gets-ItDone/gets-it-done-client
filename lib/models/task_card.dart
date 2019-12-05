f2384f2640deef7baa1edde0cc5c2b9dd8fea69a
Matt Daley <mdaleyhk@gmail.com>
2019-12-05 20:48:56 +0000

    Create card for displaying on master lister.

---
+++ lib/models/task_card.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class TaskCard extends StatelessWidget {
+  final String task;
+  TaskCard({this.task});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 8.0),
+      child: Card(
+        margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0),
+        child: ListTile(
+          leading: CircleAvatar(
+            child: ClipOval(
+                child: Image.network(
+                    'https://upload.wikimedia.org/wikipedia/commons/9/9f/Poop_Emoji_Icon.png')),
+          ),
+          title: Center(child: Text(task)),
+        ),
+      ),
+    );
+  }
+}
