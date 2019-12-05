8a49f70658bcca5e44cec7554adb40f6b3dd05ea
Matt Daley <mdaleyhk@gmail.com>
2019-12-05 20:50:27 +0000

    Render fixed data for tasks per category. Enable swipe functionality for delete.

--- lib/models/category_card.dart
+++ lib/models/category_card.dart
@@ -1,27 +1,65 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:gets_it_done/models/tasks_by_category.dart';
+import 'package:gets_it_done/models/task_card.dart';
+// import 'package:gets_it_done/models/tasks_by_category.dart';
 
 class CategoryCard extends StatefulWidget {
   final String category;
   CategoryCard({this.category});
+
   @override
   _CategoryCardState createState() => _CategoryCardState();
 }
 
 class _CategoryCardState extends State<CategoryCard> {
   bool clicked = false;
+
+  // Do call to DB here.
+  // Store data in object like below
+  dynamic data = {
+    "General": ['Eat lunch', 'Complete 3 Katas on CodeWars'],
+    "Household": ['Clean bathroom'],
+    "Health": []
+  };
+
   @override
   Widget build(BuildContext context) {
+    dynamic key = widget.category;
+
+    listData() {
+      if (data[key].length == 0)
+        return ['No Tasks for this category. Great job!']
+            .map<Widget>((item) => TaskCard(task: item))
+            .toList();
+      else {
+        return data[key]
+            .map<Widget>((item) => Dismissible(
+                  key: Key(item),
+                  onDismissed: (direction) {
+                    final index = data[key].indexOf(item);
+                    // Remove the item from the data source.
+                    setState(() {
+                      // Need to also remove from DB. Do opti-rendering
+                      data[key].removeAt(index);
+                    });
+                  },
+                  child: TaskCard(task: item),
+                ))
+            .toList();
+      }
+    }
+
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
         ButtonTheme(
-          minWidth: MediaQuery.of(context).size.width * 0.80,
+          minWidth: MediaQuery.of(context).size.width * 0.90,
+          padding: EdgeInsets.all(12.0),
           child: RaisedButton(
             color: Colors.pink[300],
             onPressed: () {
               setState(() {
                 clicked = !clicked;
-                print(clicked);
               });
             },
             child: Text(
@@ -31,8 +69,7 @@ class _CategoryCardState extends State<CategoryCard> {
           ),
         ),
         Column(
-          children: <Widget>[TasksByCategory()],
-          // children: clicked ? <List>[TasksByCategory] : [],
+          children: clicked ? listData() : [],
         )
       ],
     );
--- lib/screens/task_screens/task_list.dart
+++ lib/screens/task_screens/task_list.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-=======
 import 'package:gets_it_done/models/category_card.dart';
->>>>>>> c08d2627b9b3a30a4a5f52308d55c660599b9eb1
 
 class TaskList extends StatefulWidget {
   @override
@@ -30,33 +27,7 @@ class _TaskListState extends State<TaskList> {
           itemBuilder: (context, index) {
             final category = categories[index];
 
-<<<<<<< HEAD
-            return Column(
-              children: <Widget>[
-                ButtonTheme(
-                  minWidth: MediaQuery.of(context).size.width * 0.80,
-                  child: RaisedButton(
-                    color: Colors.pink[300],
-                    onPressed: () {
-                      setState(() {
-                        clicked = !clicked;
-                        print(clicked);
-                      });
-                    },
-                    child: Text(
-                      category,
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-                Column(
-                  children: clicked ? tasks : [],
-                )
-              ],
-            );
-=======
             return CategoryCard(category: category);
->>>>>>> c08d2627b9b3a30a4a5f52308d55c660599b9eb1
           },
         ),
       ),
