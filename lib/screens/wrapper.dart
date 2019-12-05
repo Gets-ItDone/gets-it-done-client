c08d2627b9b3a30a4a5f52308d55c660599b9eb1
Jasmine Chatfield <jazzchatfield@gmail.com>
2019-12-05 13:43:08 +0000

    all

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gets_it_done/models/user.dart';
 import 'package:gets_it_done/screens/authenticate/auth_toggle.dart';
 import 'package:gets_it_done/screens/home/home.dart';
+import 'package:gets_it_done/screens/task_screens/task_list.dart';
 import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
@@ -9,6 +10,6 @@ class Wrapper extends StatelessWidget {
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
 
-    return user != null ? Home() : AuthToggle();
+    return user != null ? TaskList() : AuthToggle();
   }
 }
