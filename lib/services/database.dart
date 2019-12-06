daa0cf6af471844773d7b0f723f6fea07791193e
j-cleary914 <j.cleary914@gmail.com>
2019-12-06 11:17:01 +0000

    add functions to update Tasks

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -12,16 +12,22 @@ class DatabaseCalls {
         "speechToText": false,
         "taskAssistant": false
       },
-      "categories": {"general": []}
+      "categories": {
+        "general": [
+          {"taskName": "clean kitchen", "completed": false},
+          {"taskName": "clean bedroom", "completed": false}
+        ]
+      }
     });
   }
 
   void addTask(uid) {
+//(#^.^#) i need some more arguments UwU
     print("this function is being called!!");
     print(uid);
 
     try {
-      //will take in category as an argument
+//will take in category as an argument
       final category = "general";
 
       testCollection.document(uid).updateData({
@@ -36,9 +42,9 @@ class DatabaseCalls {
     print("this function is being called!!");
     print(uid);
 
-    /*---^_^ this will overwrite an existing category
+    /*--- this will overwrite an existing category
     even if it has tasks in it ¯\_(ツ)_/¯---*/
-    final category = "schoolwork";
+    final category = "shopping";
     try {
       testCollection.document(uid).updateData({"categories.$category": []});
     } catch (err) {
@@ -46,14 +52,11 @@ class DatabaseCalls {
     }
   }
 
-
-  void getTasks(uid) {
+  dynamic getCategories(uid) {
     print("getting the tasks for user $uid");
 
-    testCollection.document(uid).get().then((DocumentSnapshot ds) {
-      var unknown = ds.data;
-      print(unknown);
-    });
+    return testCollection.document(uid).get();
+  }
 
   void updatePreferences(uid) {
     print("Preferences for $uid are being updated");
@@ -74,13 +77,86 @@ class DatabaseCalls {
   }
 
   void deleteUser(uid) {
-
     //need to send user back to login screen. Must discuss best way to do this
     try {
       testCollection.document(uid).delete();
-    } catch(err) {
+    } catch (err) {
       print(err);
     }
+  }
 
+  void editTask(uid) async {
+    /*
+    Hello! (=^・^=) 
+    This function currently simply takes in the current list of tasks for a hard coded category
+    and overwrites the final item in it (✿◠‿◠)
+
+    it needs more arguments to be a better function, perhaps you could help?? (#^.^#)
+    */
+
+    final category = "general";
+    final taskToUpdate = "clean kitchen";
+    final newTask = "proof it works!";
+
+    final ds = await this.getCategories(uid);
+    final currentTaskArray = ds.data["categories"][category];
+    final mappedArray = currentTaskArray.map((x) => x["taskName"]).toList();
+    final taskIndex = mappedArray.indexOf(taskToUpdate);
+    currentTaskArray[taskIndex]["taskName"] = newTask;
+
+    try {
+      print("updating data...");
+      testCollection
+          .document(uid)
+          .updateData({"categories.$category": currentTaskArray});
+    } catch (err) {
+      print(err);
+    }
+  }
+
+  void completeTask(uid) async {
+/*
+  (✿◠‿◠) Me again! category and taskName should again be taken in as arguments,
+  somebody will have to update this later >^_^< unless, perhaps, you could do it? (^_-)-☆
+*/
+    final category = "general";
+    final taskName = "clean kitchen";
+
+    final ds = await this.getCategories(uid);
+    final currentTaskArray = ds.data["categories"][category];
+    final mappedArray = currentTaskArray.map((x) => x["taskName"]).toList();
+    final taskIndex = mappedArray.indexOf(taskName);
+    currentTaskArray[taskIndex]["completed"] = true;
+
+    print(currentTaskArray);
+    try {
+      print("updating data...");
+      testCollection
+          .document(uid)
+          .updateData({"categories.$category": currentTaskArray});
+    } catch (err) {
+      print(err);
+    }
+  }
+
+  void resetTask(uid) async {
+    print('resetting task...');
+
+    final obj = [
+      {"taskName": "clean kitchen", "completed": false},
+      {"taskName": "clean bedroom", "completed": false}
+    ];
+
+    await testCollection.document(uid).updateData({"categories.general": obj});
+  }
+
+  void deleteCategory(uid) async {
+    final category = "general";
+
+    final ds = await this.getCategories(uid);
+    final currentTaskArray = ds.data["categories"][category];
+    final mappedArray = currentTaskArray.map((x) => x["taskName"]).toList();
+
+    print(mappedArray);
   }
 }
