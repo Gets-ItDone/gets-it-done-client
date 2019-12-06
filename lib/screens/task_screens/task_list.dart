6c98c3b45bfa8302ceadba5e68c1af17762fa5ab
Matt Daley <mdaleyhk@gmail.com>
2019-12-06 09:49:12 +0000

    Add bottom nav bar for moving between screens.

--- lib/screens/task_screens/task_list.dart
+++ lib/screens/task_screens/task_list.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gets_it_done/models/category_card.dart';
+import 'package:gets_it_done/screens/home/settings.dart';
+import 'package:gets_it_done/screens/task_screens/taskadder.dart';
+import 'package:gets_it_done/services/auth.dart';
 
 class TaskList extends StatefulWidget {
   @override
@@ -7,20 +10,55 @@ class TaskList extends StatefulWidget {
 }
 
 class _TaskListState extends State<TaskList> {
-  final categories = ['General', 'Household', 'Health'];
-  final generalTasks = ['Eat', 'Sleep', 'Code'];
+  //Call to DB and save data into array
+  final categories = ['General', 'Household', 'Health', 'Life or Death'];
   bool clicked = false;
 
   @override
   Widget build(BuildContext context) {
-    dynamic tasks = generalTasks.map((task) => Text(task)).toList();
+    final AuthService _auth = AuthService();
+    void _navigatePage(int index) {
+      setState(() {
+        if (index == 0) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => TaskAdder()),
+          );
+        }
+        if (index == 1) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Settings()),
+          );
+        }
+        if (index == 2) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Settings()),
+          );
+        }
+      });
+    }
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Master List'),
         backgroundColor: Colors.pink[300],
+        actions: <Widget>[
+          FlatButton(
+            child: Text(
+              'Log Off',
+              style: TextStyle(color: Colors.white, fontSize: 18.0),
+            ),
+            onPressed: () async {
+              print('Sign out');
+              await _auth.logOffUser();
+            },
+          )
+        ],
       ),
       body: Container(
+        padding: EdgeInsets.fromLTRB(0, 40.0, 0, 40.0),
         color: Colors.blue[200],
         child: ListView.builder(
           itemCount: categories.length,
@@ -31,6 +69,31 @@ class _TaskListState extends State<TaskList> {
           },
         ),
       ),
+      bottomNavigationBar: Theme(
+          data: Theme.of(context).copyWith(
+            canvasColor: Colors.pink[300],
+            primaryColor: Colors.white,
+            textTheme: Theme.of(context).textTheme.copyWith(
+                  caption: new TextStyle(color: Colors.white),
+                ),
+          ),
+          child: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.add),
+                title: Text('Add Task'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.done),
+                title: Text('Do Task'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                title: Text('Settings'),
+              ),
+            ],
+            onTap: _navigatePage,
+          )),
     );
   }
 }
