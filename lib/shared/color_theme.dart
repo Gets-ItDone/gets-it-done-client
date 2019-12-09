f37e21e8315184aee3843d8a75fe8182426b7424
Matt Daley <mdaleyhk@gmail.com>
2019-12-09 21:57:04 +0000

    Update color schemes.

--- lib/shared/color_theme.dart
+++ lib/shared/color_theme.dart
@@ -17,19 +17,43 @@ getColorTheme(colorScheme) {
 
   if (colorScheme == '2') {
     return ThemeData(
-      backgroundColor: Colors.red,
+      brightness: Brightness.dark,
+      primaryColor: Colors.red[600],
+      accentColor: Colors.redAccent[400],
+      scaffoldBackgroundColor: Colors.yellow[600],
+      backgroundColor: Colors.yellow[600],
+      buttonColor: Colors.red[600],
+      bottomAppBarColor: Colors.yellow[600],
+      hintColor: Colors.red[600],
+      fontFamily: 'Roboto',
     );
   }
 
   if (colorScheme == '3') {
     return ThemeData(
-      backgroundColor: Colors.red,
+      brightness: Brightness.dark,
+      primaryColor: Colors.pink[400],
+      accentColor: Colors.pinkAccent[400],
+      scaffoldBackgroundColor: Colors.purple[300],
+      backgroundColor: Colors.purple[300],
+      buttonColor: Colors.pink[400],
+      bottomAppBarColor: Colors.pink[400],
+      hintColor: Colors.purple[300],
+      fontFamily: 'Roboto',
     );
   }
 
   if (colorScheme == '4') {
     return ThemeData(
-      backgroundColor: Colors.red,
+      brightness: Brightness.dark,
+      primaryColor: Colors.white,
+      accentColor: Colors.blue,
+      scaffoldBackgroundColor: Colors.blue[300],
+      backgroundColor: Colors.blue[300],
+      buttonColor: Colors.blue[900],
+      bottomAppBarColor: Colors.white,
+      hintColor: Colors.blue[300],
+      fontFamily: 'Roboto',
     );
   }
 }
