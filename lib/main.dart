8ac157b4b6474baa38ad191beb2266cbd8fb64c8
Matt Daley <mdaleyhk@gmail.com>
2019-12-09 19:17:15 +0000

    Add theme to app.

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,26 @@ import 'package:gets_it_done/screens/wrapper.dart';
 import 'package:gets_it_done/services/auth.dart';
 import 'package:provider/provider.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return StreamProvider<User>.value(
       value: AuthService().user,
-      child: MaterialApp(home: Wrapper()),
+      child: MaterialApp(
+          theme: ThemeData(
+            brightness: Brightness.dark,
+            primaryColor: Colors.green[600],
+            accentColor: Colors.greenAccent[400],
+            scaffoldBackgroundColor: Colors.black,
+            backgroundColor: Colors.black,
+            buttonColor: Colors.green[600],
+            bottomAppBarColor: Colors.green[600],
+            hintColor: Colors.green[600],
+            fontFamily: 'Roboto',
+          ),
+          home: Wrapper()),
     );
   }
 }
